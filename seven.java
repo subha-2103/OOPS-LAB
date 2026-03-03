0d40bbb82e89747e7e0caca3c143a96e4ad33754
subha-2103 <112534121@kanchiuniv.ac.in>
2026-03-03 01:02:35 -0500

    Add files via upload

---
+++ seven.java
@@ -0,0 +1,18 @@
+impart java until*;
+class seven
+{
+static public void main(String[] args)throws Exception
+String input = new Scanner (system.in);
+String s1,s2;
+System.out.print("Enter First string");
+s1=input nextline ();
+System.out.print("Enter Second string");
+s2=input nextline ();
+if (s1==s2)
+}
+System.out.print("TWO STRING ARE EQUAL");
+}
+if(s1 !=s2)
+{
+System.out.print("TWO STRING ARE NOT EQUAL");
+}}}
