682406181b4bbe405a43ef66044510ada86a4566
subha-2103 <112534121@kanchiuniv.ac.in>
2026-03-17 10:19:34 +0530

    Add files via upload

---
+++ four.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class four
+{
+  static public void main(String[]args)throws Exception
+{
+  Scanner s1 = new Scanner(System.in);
+  System.out.print("enter your name:");
+  String s2 = s1.nextLine();
+  System.out.print("your name is"+s2);
+}
+}
