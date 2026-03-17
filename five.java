7cf3fffce71f2df0ea06c4f1bfd42fe28bfdc436
subha-2103 <112534121@kanchiuniv.ac.in>
2026-03-17 10:19:50 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class five
+{
+static public void main(String[]args)throws exception
+{
+String s1,s2;
+int i1,i2;
+Scanner input=new Scanner (System.in);
+System.out.println("Enter the first value");
+s1=input.nextLine();
+i1=IntegerPassaint (s1);
+System.out.println("Enter the second value");
+s2=input.nextLine();
+i2=integerPassaint (Sc);
+if((i1%i2)==0)
+{
+System.out.println("the Given number is even");
+}
+System.out.println("the Given number is odd");
+}
+}
+
