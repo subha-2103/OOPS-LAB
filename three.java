b09890232f79d93e2c1277d0bb548f3b196979d7
subha-2103 <112534121@kanchiuniv.ac.in>
2026-03-17 10:19:12 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,17 @@
+class three
+{
+static public void main (String[]args)
+{
+int a,b,c;
+a=20;
+b=10;
+c=a+b;
+System.out.println("value of c is"+c);
+c=a-b;
+System.out.println("value of c is"+c);
+c=a*b;
+System.out.println("value of c is"+c);
+c=a/b;
+System.out.println("value of c is"+c);
+}
+}
