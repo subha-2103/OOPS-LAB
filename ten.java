5ccaf80d5803cde3f23d74e8eb001e8f9caa89d3
subha-2103 <112534121@kanchiuniv.ac.in>
2026-03-24 11:00:53 +0530

    Add files via upload

---
+++ ten.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ten
+{
+static public void main(String[]args)
+{
+int x,y;
+Scanner s1 = new Scanner(System.in);
+System.out.println("enter x value:");
+x=Integer.parseInt(s1.nextLine());
+System.out.println("enter y value:");
+y=Integer.parseInt(s1.nextLine());
+if(x>y)
+{
+System.out.println("x is greater than y");
+}
+else
+{
+System.out.println("y is greater than x");
+}
+}}
